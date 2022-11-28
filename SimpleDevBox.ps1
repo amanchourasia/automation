b826ecca509f3d75dd151b21f67ed6477e1c1d13
amanchourasia <40295937+amanchourasia@users.noreply.github.com>
2022-11-28 18:15:13 +0530

    Update SimpleDevBox.ps1

--- SimpleDevBox.ps1
+++ SimpleDevBox.ps1
@@ -20,7 +20,13 @@ Disable-IEESC
 
 #cd c://; mkdir automation; cd automation; git clone https://username:password@github.com/amanchourasia/automation.git; Copy-Item -Path C:\automation\automation-vm\Certs -Destination C:\
 
-cd c://; mkdir automation; cd automation; $MyPat = 'cut4c4xaty6upvi4ve2ext4lirhrlpmuqannsypkxj5edvruyywq; $B64Pat = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("`:$MyPat")); git -c http.extraHeader="Authorization: Basic $B64Pat" clone https://rimilia@dev.azure.com/rimilia/Alloc8/_git/automation-vm; Copy-Item -Path C:\automation\automation-vm\Certs -Destination C:\
+cd c://; mkdir automation; cd automation; 
+
+$MyPat = 'cut4c4xaty6upvi4ve2ext4lirhrlpmuqannsypkxj5edvruyywq'
+$B64Pat = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("`:$MyPat"))
+git -c http.extraHeader="Authorization: Basic $B64Pat" clone https://rimilia@dev.azure.com/rimilia/Alloc8/_git/automation-vm
+
+Copy-Item -Path C:\automation\automation-vm\Certs -Destination C:\
 
 # cd c://
 # mkdir automation
